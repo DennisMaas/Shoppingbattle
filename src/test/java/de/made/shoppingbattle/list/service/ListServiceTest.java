da48904742783921d7c56bf97da7042c71cb54ee
DennisMaas <mail@dennismaas.de>
2021-05-06 14:24:56 +0200

    add empty test class

---
+++ src/test/java/de/made/shoppingbattle/list/service/ListServiceTest.java
@@ -0,0 +1,19 @@
+package de.made.shoppingbattle.list.service;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ListServiceTest {
+
+    @Test
+    @DisplayName("Returns all Items")
+    void itShouldGetAllItems() {
+        //GIVEN
+
+        //WHEN
+
+        //THEN
+    }
+}
