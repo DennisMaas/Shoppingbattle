7d6e968f078ece251949ba72e0e981b7dbe90869
DennisMaas <mail@dennismaas.de>
2021-05-06 13:33:59 +0200

    Initial commit

---
+++ src/main/java/de/made/shoppingbattle/ShoppingbattleApplication.java
@@ -0,0 +1,13 @@
+package de.made.shoppingbattle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingbattleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShoppingbattleApplication.class, args);
+    }
+
+}
