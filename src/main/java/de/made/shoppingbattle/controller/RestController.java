8f2b8907de1cd800b25b789b4e57611122ba09e3
DennisMaas <mail@dennismaas.de>
2021-05-06 13:53:26 +0200

    add GET request to RestController

---
+++ src/main/java/de/made/shoppingbattle/controller/RestController.java
@@ -0,0 +1,16 @@
+package de.made.shoppingbattle.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@org.springframework.web.bind.annotation.RestController
+@RequestMapping("api/v1/")
+public class RestController {
+
+    @GetMapping("/")
+    List<Item> getAllItems(String user) {
+        return listService.getAllItems(user);
+    }
+
+ }
---
+++ src/test/java/de/made/shoppingbattle/controller/RestControllerTest.java
@@ -0,0 +1,8 @@
+package de.made.shoppingbattle.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RestControllerTest {
+
+
+}
